e337c763a6b526ca983d39cd1d803cd9ab30d2f0
SEUNGYONG KIM <sseungyong@gmail.com>
2020-09-13 22:27:46 +0900

    fixed error

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -12,7 +12,12 @@ const GET_MOVIES = gql`
 `;
 
 export default () => {
-  const { loading, error, data } = useQuery(GET_MOVIES);
-  console.log(loading, error, data);
-  return <h1>Home</h1>;
+  const { loading, data } = useQuery(GET_MOVIES);
+  // console.log(loading, error, data);
+  if (loading) {
+    return <h1>"loading..."</h1>;
+  }
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1>{m.id}</h1>);
+  }
 };
