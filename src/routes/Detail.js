627e44ca769d6c20201611e4baf7c8124b8cc88a
SEUNGYONG KIM <sseungyong@gmail.com>
2020-09-14 23:51:12 +0900

    add local state

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,89 @@
-export default () => "Detail";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+import styled from "styled-components";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    # for Apollo
+    movie(id: $id) {
+      # for server
+      id
+      title
+      rating
+      language
+      medium_cover_image
+      description_intro
+      isLiked @client
+    }
+    suggestions(id: $id) {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 100px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 25px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+  margin-right: 100px;
+`;
+
+export default () => {
+  let { id } = useParams();
+  id = parseInt(id);
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id },
+  });
+  console.log(data);
+  return (
+    <Container>
+      <Column>
+        <Title>
+          {loading ? "Loading..." : `${data.movie.title} ${data.movie.isLiked ? "♥" : "♨"}`}
+        </Title>
+        <Subtitle>
+          {data?.movie?.language} · {data?.movie?.rating}
+        </Subtitle>
+        <Description>{data?.movie?.description_intro}</Description>
+      </Column>
+      <Poster></Poster>
+      {/* <Poster bg={data && data.movie ? data.movie.medium_cover_image : ""}></Poster> */}
+      <Poster bg={data?.movie?.medium_cover_image}></Poster>
+    </Container>
+  );
+};
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,23 +1,91 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
+import Movie from "../components/Movie";
 
 const GET_MOVIES = gql`
-  {
-    movies {
+  query getMovies($rating: Float!) {
+    # for Apollo
+    movies(rating: $rating) {
       id
       medium_cover_image
+      isLiked @client
     }
   }
 `;
 
+// const GET_MOVIES = gql`
+//   {
+//     movies {
+//       id
+//       medium_cover_image
+//     }
+//   }
+// `;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
 export default () => {
-  const { loading, data } = useQuery(GET_MOVIES);
-  // console.log(loading, error, data);
-  if (loading) {
-    return <h1>"loading..."</h1>;
-  }
-  if (data && data.movies) {
-    return data.movies.map((m) => <h1>{m.id}</h1>);
-  }
+  const { loading, error, data } = useQuery(GET_MOVIES, {
+    variables: { rating: 8.9 },
+  });
+  console.log(loading, error, data);
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo 2020</Title>
+        <Subtitle>I love GraphQL</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      <Movies>
+        {data?.movies?.map((m) => (
+          <Movie
+            key={m.id}
+            id={m.id}
+            isLiked={m.isLiked}
+            bg={m.medium_cover_image ? m.medium_cover_image : ""}
+          />
+        ))}
+      </Movies>
+    </Container>
+  );
 };
